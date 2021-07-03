7f5e480e95bb1eee1db6c96e1979d81b3df53d86
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-07-03 11:30:32 +0530

    3sum.cpp O(n^2)

--- Day 4/3sum.cpp
+++ Day 4/3sum.cpp
@@ -29,3 +29,44 @@ public:
     }
 };
 
+// O(N^2)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& arr) {
+        int n = arr.size();
+        if(n < 3)
+            return {};
+        
+        vector<vector<int>> ans;
+        
+        sort(arr.begin() , arr.end());
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(i > 0 && arr[i] == arr[i-1])
+                continue;
+            
+            int low = i+1, high = n-1;
+            
+            while(low < high)
+            {
+                int sum = arr[i] + arr[low] + arr[high];
+                if(sum < 0)
+                    low++;
+                else if(sum > 0)
+                    high--;
+                else
+                {
+                    ans.push_back({arr[i] , arr[low] , arr[high]});
+                    
+                    while(low < high && arr[low] == arr[low+1]) low++;
+                    while(low < high && arr[high] == arr[high-1]) high--;
+                    low++;
+                    high--;
+                }
+            }
+                
+        }
+        return ans;
+    }
+};
