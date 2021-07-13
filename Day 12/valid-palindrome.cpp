d6d1a6494df0ed09862daccfe45b66a9a33859b4
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-07-14 02:01:40 +0530

    valid-palindrome

---
+++ Day 12/valid-palindrome.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        for(int i=0,j=s.size()-1;i<j;)
+        {
+            if(!isalnum(s[i]))
+            {
+                i++;
+                continue;
+            }
+            if(!isalnum(s[j]))
+            {
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j]))
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
