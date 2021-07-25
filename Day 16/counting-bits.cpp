1a48ea11c5d73294f819f40624754a0e98222681
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-07-25 08:15:09 +0530

    counting-bits

---
+++ Day 16/counting-bits.cpp
@@ -0,0 +1,16 @@
+
+// https://leetcode.com/problems/counting-bits/
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        //mem[i] = No of 1s from 0 to number i
+        vector<int> mem(num+1);
+        mem[0] = 0;
+        
+        for(int i=1;i<=num;++i)
+            mem[i] = mem[i/2] + i%2; //for odd add 1
+        
+        return mem;
+    }
+};
