4cf3db9e3ecb921179d9086de5134103e12a5d79
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-08-12 11:29:18 +0530

    kadanes-algorithm

---
+++ Day 29/kadanes-algorithm.cpp
@@ -0,0 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1#
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+// Function to find subarray with maximum sum
+// arr: input array
+// n: size of array
+int maxSubarraySum(int arr[], int n){
+    
+    // Your code her
+    int curr_sum=arr[0],max_sum=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        curr_sum=max(arr[i]+curr_sum,arr[i]);
+        max_sum=max(max_sum,curr_sum);
+        
+    }
+    return max_sum;
+    
+    
+}
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+        
+        cout << maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
