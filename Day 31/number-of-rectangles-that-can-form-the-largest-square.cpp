2a5a4cb264544d91782d3d9898f35ceb66e1c464
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-08-18 00:17:37 +0530

    number-of-rectangles-that-can-form-the-largest-square

---
+++ Day 31/number-of-rectangles-that-can-form-the-largest-square.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+
+class Solution {
+public:
+    int countGoodRectangles(vector<vector<int>>& rectangles) {
+        unordered_map<int,int> map;
+        for(vector<int> rectangle: rectangles){
+		    
+            map[min(rectangle[0],rectangle[1])]++; 
+        }
+        int cnt=INT_MIN,maxlen=INT_MIN;
+        for(auto m: map){
+            if(m.first>maxlen) cnt=m.second,maxlen=m.first; 
+        }
+        return cnt;
+    }
+};
